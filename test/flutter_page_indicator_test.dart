b4c1eeb67bc94af866559bfd4b5518674d1b0ab9
JarvanMo <jarvan.mo@gmail.com>
2019-07-29 20:31:03 +0800

    convert this library to flutter_page_indicators

--- test/flutter_page_indicator_test.dart
+++ test/flutter_page_indicator_test.dart
@@ -1,6 +1,6 @@
 import 'package:test/test.dart';
 
-import 'package:flutter_page_indicator/flutter_page_indicator.dart';
+import 'package:flutter_page_indicator/flutter_page_indicators.dart';
 
 void main() {
   test('adds one to input values', () {});
